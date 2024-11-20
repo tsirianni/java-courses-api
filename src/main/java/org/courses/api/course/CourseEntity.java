467fa6ff6320356c73cca08463624ac1173ec09b
Tobias <sirianni.gf@gmail.com>
2024-11-20 15:10:24 -0300

    fix: drop default values on course entity

--- src/main/java/org/courses/api/course/CourseEntity.java
+++ src/main/java/org/courses/api/course/CourseEntity.java
@@ -1,8 +1,14 @@
 package org.courses.api.course;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -10,11 +16,20 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "course")
 public class CourseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
     private short courseCategoryId;
     private boolean active;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 }
